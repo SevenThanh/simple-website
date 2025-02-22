ef956e2b1fafd33b568bc0c5e3838380d2c8b09b
Johan Nguyen <thaithanhace@gmail.com>
2025-02-21 20:08:32 -0500

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,8 +9,21 @@ function Main() {
         <p>
           I created this simple website for you to get to know me both{' '}
           <Link to="/professionally">professionally</Link> and{' '}
-          <Link to="/personally">personally</Link> (preferably both because I think I'm pretty awesome 😊)
+          <Link to="/personally">personally</Link> (preferably both because I think I'm pretty awesome sauce😊)
         </p>
+        <p>
+        I’d like to sincerely thank Dr. Jason Rabinovitch, Dr. Virginia Con, and Dr. Philippe Meunier for agreeing to write me a letter of recommendation. 
+        </p>
+        <p>
+        To you, it may just be a letter, but to me, it’s a chance to build a brighter future—one where I can help my grandma with her cancer treatment, 
+        support my mom with her rent, assist my relatives in Vietnam with their farming work, and give kids in underprivileged areas access to the education they deserve.
+        </p>
+        <div className="first-page-imgages">
+            <img src="cat1.JPG" alt="First placeholder" />
+            <img src="cat2.JPG" alt="Second placeholder" />
+            <img src="cat3.jpg" alt="Third placeholder" />
+        </div>
+        <p className='cat'> Top 3 Cats of All Time</p>
       </div>
     </div>
   );
