e3ad99ebde755f240ba1bca67e53dfc5b22b0178
Johan Nguyen <thaithanhace@gmail.com>
2025-02-22 17:58:35 -0500

    update

--- src/components/professionally/Exam.js
+++ src/components/professionally/Exam.js
@@ -1,10 +1,53 @@
 import '../../styles/exam.css'
 
-
-function Exam () {
+function Exam() {
+    const exams = [
+        {
+            id: 1,
+            img: '/Exam00.jpg'
+        },
+        {
+            id: 2,
+            img: '/Exam01.jpg'
+        },
+        {
+            id: 3,
+            img: '/Exam03.jpg'
+        },
+        {
+            id: 4,
+            img: '/Exam04.jpg'
+        },
+        {
+            id: 5,
+            img: '/Exam05.jpg'
+        },
+        {
+            id: 6,
+            img: '/Exam06.jpg'
+        },
+        {
+            id: 7,
+            img: '/Exam8.jpg'
+        },
+        {
+            id: 8,
+            img: '/Exam9.jpg'
+        }
+    ];
+    
     return (
-        <div>
-
+        <div className='exam-section'>
+            <h2>
+                3. Times when I didn't have to work 25 hours a week to help with my tuition, and my friends' meal swipes came in clutch
+            </h2>
+            <div className="exam-grid">
+                {exams.map(exam => (
+                    <div key={exam.id} className="exam-item">
+                        <img src={exam.img} alt={`Exam ${exam.id}`} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/professionally/Professionally.js
+++ src/components/professionally/Professionally.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Experience from './Experience';
 import Projects from './Projects';
+import Exam from './Exam';
 
 function Professionally() {
   return (
     <div>
       <Experience />
       <Projects />
+      <Exam />
     </div>
   );
 }
