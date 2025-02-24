ae79573d246ec20cc111c8e3c9a4f3c37ccb6b4d
Johan Nguyen <thaithanhace@gmail.com>
2025-02-23 22:53:07 -0500

    done

--- src/components/personally/Personally.js
+++ src/components/personally/Personally.js
@@ -1,10 +1,15 @@
 import Aboutme from "./Aboutme";
 import Hobbies from "./Hobbies";
 import Why from "./Why";
+import '../../styles/navigation.css'
+import { Link } from 'react-router-dom'; 
 
 function Personally() {
     return (
       <div className="content-section">
+      <Link to="/professionally" className="nav-button">
+        View Professional Side
+      </Link>
         <Aboutme/>
         <Hobbies />
         <Why/>
--- src/components/professionally/Professionally.js
+++ src/components/professionally/Professionally.js
@@ -3,9 +3,13 @@ import Experience from './Experience';
 import Projects from './Projects';
 import Exam from './Exam';
 
+import { Link } from 'react-router-dom'; 
 function Professionally() {
   return (
     <div>
+      <Link to="/personally" className="nav-button">
+        View Personal Side
+      </Link>
       <Experience />
       <Projects />
       <Exam />
